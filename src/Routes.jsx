ea4796c52c4e8563b412a82f9b3fdb0c30a492af
sumit kumar <sumi9998877@gmail.com>
2023-03-28 15:58:46 +0530

    push code

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,21 +1,17 @@
-// import Home from "pages/Home";
+import Main2 from "pages/Main";
 import NotFound from "pages/NotFound";
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-const Main = React.lazy(() => import("pages/Main"));
-const Home = React.lazy(() => import("pages/Home"));
+
 const ProjectRoutes = () => {
   return (
-    <React.Suspense fallback={<>Loading...</>}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="*" element={<NotFound />} />
-          {/* <Route path="/home" element={<Home />} /> */}
-          <Route path="/main" element={<Main />} />
-        </Routes>
-      </Router>
-    </React.Suspense>
+    // <React.Suspense >
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main2 />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+    // </React.Suspense>
   );
 };
 export default ProjectRoutes;
